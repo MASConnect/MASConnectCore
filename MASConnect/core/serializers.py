6002721cfcbbfe593a522637bc84f81e382facaf
arkenibrahim@live.com <arken@arken.com>
2019-03-06 23:20:17 -0500

    changes

--- MASConnect/core/serializers.py
+++ MASConnect/core/serializers.py
@@ -9,3 +9,7 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         exclude = ('password',)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
